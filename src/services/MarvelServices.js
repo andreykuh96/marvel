0461d56a42a2b4463d044d48fd78e755537a1b35
andrey <andrey.kuh96@mail.ru>
2023-02-25 18:13:57 +0300

    loading a random character

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -1,4 +1,7 @@
 class MarvelServices {
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=8593fef007768c69bd7d62745df90ef1';
+
 	getResource = async (url) => {
 		const res = await fetch(url);
 
@@ -10,7 +13,23 @@ class MarvelServices {
 	}
 
 	getAllCharacters = () => {
-		return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=250&apikey=8593fef007768c69bd7d62745df90ef1');
+		return this.getResource(`${this._apiBase}characters?limit=9&offset=250&${this._apiKey}`);
+	}
+
+	getCharacter = async (id) => {
+		const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+		return this._transformCharacter(res.data.results[0]);
+	}
+
+	_transformCharacter = (char) => {
+		console.log(char.description.length);
+		return {
+			name: char.name,
+			description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+		}
 	}
 }
 
